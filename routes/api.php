9e8ae016d25ab5bc3120b94d6e05606ced8d096b
stefGoss <m1.gouguasse@gmail.com>
2019-07-18 14:35:16 +0100

    migration made for tables

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('/products','ProductController');
 Route::group(['prefix'=>'products'],function(){
+    
     Route::apiResource('/{product}/reviews','ReviewController');
+
 });
 
 
